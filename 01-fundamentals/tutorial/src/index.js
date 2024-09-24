55620837a3cf97feeed5ad66facd7cb98fcdc5bb
Muhammad Ramzan Akram <ramzanakram1875@outlook.com>
2024-09-24 20:13:09 +0500

    Learn till Naming convention in jsx

--- 01-fundamentals/tutorial/src/index.js
+++ 01-fundamentals/tutorial/src/index.js
@@ -2,30 +2,107 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+//First Component in Detail-Topic 01
+
 // Creating our first component
 
-function Greeting() {
-  return <h1>My First component</h1>;
-}
+// function Greeting() {
+//   return (
+//     <div>
+//       <h1>My First component</h1>
+//     </div>
+//   );
+// }
 
-export default Greeting;
+// export default Greeting;
 
 //We can also do this via arrow function
 
-// const Greetings = ()=>{
-//     return(
-//         <h1>My First component</h1>
-//     )
-// }
+// const Greeting = () => {
+//   return <h1>My First component</h1>;
+// };
 
-// export default Greetings
+// export default Greeting;
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+// const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<Greeting />);
+// root.render(<Greeting />);
 
 /*Components rules
 - starts with capital letter
 - must return JSX (html)
 - always close tag <Greeting/>
+*/
+
+//There is another way we can create element function
+//First Way
+
+// function Greeting() {
+//   return React.createElement("h2", {}, "Hello World");
+// }
+
+//Second Way
+
+// const Greeting = () => {
+//   return React.createElement(
+//     "div",
+//     {},
+//     React.createElement("h2", {}, "Hello World")
+//   );
+// };
+
+// export default Greeting;
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(<Greeting />);
+
+//JSX Rules-Topic 02
+
+//We have one parent element in the react component. For examples as following
+//1-<div></div>
+//2-<section></section>
+//3-<React.fragment></React.fragment>
+//4-<></>
+
+function Greeting() {
+  return (
+    <section>
+      <h3>Hello People</h3>
+      <ul>
+        <li>
+          <a href="#">Hello World</a>
+        </li>
+      </ul>
+      <h2>Hello World</h2>
+    </section>
+  );
+}
+
+export default Greeting;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<Greeting />);
+
+//We use camelCase names in naming conventions
+//We use html tags attributes here as in camelCase naming convention not same as we use in html for example below tags,
+
+/*In jsx we use like below,
+return (
+  <div tabIndex={1}>
+    <button onClick={myFunction}>click me</button>
+    <label htmlFor='name'>Name</label>
+    <input readOnly={true} id='name' />
+  </div>
+)
+*/
+
+/*
+// in html
+<div tabindex="1">
+    <button onclick="myFunction()">click me</button>
+    <label for='name'>Name</label>
+    <input readonly id='name' />
+</div>
 */
